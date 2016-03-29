a9b70098cd629bbc9a8d1d3292363e2e723eed7a
geans <geans.santos@gmail.com>
2016-03-29 15:32:58 -0300

    Utilizando classe Desenho_gl para ajudar a criar objetos e diminuir número de linhas repetidas.

--- desenha_predio.cpp
+++ desenha_predio.cpp
@@ -1,12 +1,19 @@
 #include "desenho.h"
 
 unsigned int Desenho::desenha_predio(float pos_x, float pos_y, float pos_z) {
-    float cor_parede[3] = {0xa3/256.0, 0x90/256.0, 0x7f/256.0};
-    float parede_largura = 0.1;
-    float frontal_comprimento = 20.0, frontal_altura = 5.0;
-    float lateral_comprimento = 7.0, lateral_altura = 3.0;
-    float desloc_x, desloc_y, desloc_z;
     float proporcao = 2;
+    Desenha_gl gl (pos_x, pos_y, pos_z, proporcao);
+
+    float cor_parede[3] = {0xa3/256.0, 0x90/256.0, 0x7f/256.0};
+    
+    float parede_largura = 0.1;
+    float frontal_comprimento = 15.0, frontal_altura = 6.5;
+    float lateral_comprimento = 7.0, lateral_altura = 5.0;
+    float entrada_garagem_altura = 2.7;
+    float entrada_garagem_largura = 3;
+    float portao_garagem_altura = 2.2;
+    float portao_garagem_lagura = 2;
+    float portao_garagem_espessura = 0.05;
 
     /* inicia a composicao do predio */
     unsigned int predio = glGenLists (1);
@@ -20,50 +27,37 @@ unsigned int Desenho::desenha_predio(float pos_x, float pos_y, float pos_z) {
         glVertex3f ( 100, 0,  100);
         glVertex3f ( 100, 0, -100);
     glEnd();
-
     
     // parede frontal
     glColor3f ( cor_parede[0], cor_parede[1], cor_parede[2] );
-    glPushMatrix();
-    desloc_x = pos_x + 0;
-    desloc_y = pos_y + proporcao * frontal_altura / 2.0;
-    desloc_z = pos_z + 0;
-    glTranslatef (desloc_x, desloc_y, desloc_z);
-    glScalef (frontal_comprimento, frontal_altura, parede_largura);
-    glutSolidCube (proporcao);
-    glPopMatrix();
+    gl.define_deslocamento (0, frontal_altura / 2, 0);
+    gl.define_escala (frontal_comprimento, frontal_altura, parede_largura);
+    gl.define_rotacao (0, 0, 0, 0);
+    gl.desenha_cubo();
 
     // parede de fundo
-    glPushMatrix();
-    desloc_x = pos_x + 0;
-    desloc_y = pos_y + proporcao * lateral_altura / 2.0;
-    desloc_z = pos_z - proporcao * lateral_comprimento;
-    glTranslatef (desloc_x, desloc_y, desloc_z);
-    glScalef (frontal_comprimento, lateral_altura, parede_largura);
-    glutSolidCube (proporcao);
-    glPopMatrix();
+    gl.define_deslocamento (0, lateral_altura / 2, -lateral_comprimento);
+    gl.define_escala (frontal_comprimento, lateral_altura, parede_largura);
+    gl.define_rotacao (0, 0, 0, 0);
+    gl.desenha_cubo();
     
     // parede lateral esquerda
-    glPushMatrix();
-    desloc_x = pos_x - proporcao * frontal_comprimento / 2.0;
-    desloc_y = pos_y + proporcao * lateral_altura / 2.0;
-    desloc_z = pos_z - proporcao * lateral_comprimento / 2.0;
-    glTranslatef (desloc_x, desloc_y, desloc_z);
-    glRotatef (90, 0, 1, 0);
-    glScalef (lateral_comprimento, lateral_altura, parede_largura);
-    glutSolidCube (proporcao);
-    glPopMatrix();
+    gl.define_deslocamento (-frontal_comprimento / 2, lateral_altura / 2, -lateral_comprimento / 2);
+    gl.define_escala (lateral_comprimento, lateral_altura, parede_largura);
+    gl.define_rotacao (90, 0, 1, 0);
+    gl.desenha_cubo();
 
     // parede lateral direita
-    glPushMatrix();
-    desloc_x = pos_x + proporcao * frontal_comprimento / 2.0;
-    desloc_y = pos_y + proporcao * lateral_altura / 2.0;
-    desloc_z = pos_z - proporcao * lateral_comprimento / 2.0;
-    glTranslatef (desloc_x, desloc_y, desloc_z);
-    glRotatef (90, 0, 1, 0);
-    glScalef (lateral_comprimento, lateral_altura, parede_largura);
-    glutSolidCube (proporcao);
-    glPopMatrix();
+    gl.define_deslocamento (frontal_comprimento / 2, lateral_altura / 2, -lateral_comprimento/2);
+    gl.define_escala (lateral_comprimento, lateral_altura, parede_largura);
+    gl.define_rotacao (90, 0, 1, 0);
+    gl.desenha_cubo();
+
+    // entrada da garagem
+    gl.define_deslocamento (frontal_comprimento / 2 + entrada_garagem_largura / 2, entrada_garagem_altura / 2, 0);
+    gl.define_escala (entrada_garagem_largura, entrada_garagem_altura, parede_largura);
+    gl.define_rotacao (0, 0, 0, 0);
+    gl.desenha_cubo();
 
     glEndList();
     return predio;
