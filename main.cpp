1c33ad3b3252e09d546b7fcbd5bd0abe3bc3a960
geans <geans.santos@gmail.com>
2016-03-30 18:47:17 -0300

    Pego função para adicionar iluminação, mas ainda não está sendo chamada.

--- main.cpp
+++ main.cpp
@@ -31,6 +31,50 @@ void remodelar (int largura, int altura){
     glMatrixMode (GL_MODELVIEW);
 }
 
+
+// Referência: http://www.inf.pucrs.br/~manssour/OpenGL/Iluminacao.html
+//iluminacao
+void iluminacao_externa(void)
+{
+    GLfloat luzAmbiente[4]={0.2,0.2,0.2,1.0}; 
+    GLfloat luzDifusa[4]={0.7,0.7,0.7,1.0};    // "cor" 
+    GLfloat luzEspecular[4]={1.0, 1.0, 1.0, 1.0};// "brilho" 
+    GLfloat posicaoLuz[4]={0.0, 50.0, 50.0, 1.0};
+
+    // Capacidade de brilho do material
+    GLfloat especularidade[4]={1.0,1.0,1.0,1.0}; 
+    GLint especMaterial = 60;
+
+    // Especifica que a cor de fundo da janela será preta
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    // Habilita o modelo de colorização de Gouraud
+    glShadeModel(GL_SMOOTH);
+
+    // Define a refletância do material 
+    glMaterialfv(GL_FRONT,GL_SPECULAR, especularidade);
+    // Define a concentração do brilho
+    glMateriali(GL_FRONT,GL_SHININESS,especMaterial);
+
+    // Ativa o uso da luz ambiente 
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
+
+    // Define os parâmetros da luz de número 0
+    glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente); 
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa );
+    glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular );
+    glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz );
+
+    // Habilita a definição da cor do material a partir da cor corrente
+    glEnable(GL_COLOR_MATERIAL);
+    //Habilita o uso de iluminação
+    glEnable(GL_LIGHTING);  
+    // Habilita a luz de número 0
+    glEnable(GL_LIGHT0);
+    // Habilita o depth-buffering
+    glEnable(GL_DEPTH_TEST);
+}
+
 void exibir(void){
     if (deltaMove) { // mover camera para frente e traz
         x += deltaMove * vetor_x;
@@ -176,6 +220,7 @@ int main (int argc,char **argv){
     glutDisplayFunc (exibir);
     glutReshapeFunc (remodelar);
     glutIdleFunc (exibir);
+    //iluminacao_externa();
 
     glutMainLoop();
 
