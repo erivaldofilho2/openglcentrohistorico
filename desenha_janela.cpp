9d7618f379385213d7b97122367b58ebd325cf38
geans <geans.santos@gmail.com>
2016-04-05 19:38:03 -0300

    Passa a utiliza classe Desenho para organizar códiog e diminuir linhas de código

--- desenha_janela.cpp
+++ desenha_janela.cpp
@@ -5,23 +5,18 @@ unsigned int Desenho::desenha_janela (float pos_x, float pos_y, float pos_z) {
     float janela_largura = 0.05;
     float janela_comprimento = 1.0;
     float janela_altura = 3.0;
-    float desloc_x, desloc_y, desloc_z;
-    float proporcao = 2;
+    Desenha_gl gl (pos_x, pos_y, pos_z, proporcao);
 
     /* inicia a composicao da janela */
     unsigned int janela = glGenLists (1);
     glNewList (janela, GL_COMPILE);
 
-    // parede frontal
+    // janela
     glColor3f ( cor_janela[0], cor_janela[1], cor_janela[2] );
-    glPushMatrix();
-    desloc_x = pos_x + 0;
-    desloc_y = pos_y + proporcao * janela_altura / 2.0;
-    desloc_z = pos_z + 0;
-    glTranslatef (desloc_x, desloc_y, desloc_z);
-    glScalef (janela_comprimento, janela_altura, janela_largura);
-    glutSolidCube (proporcao);
-    glPopMatrix();
+    gl.define_deslocamento (0, janela_altura / 2, 0);
+    gl.define_escala (janela_comprimento, janela_altura, janela_largura);
+    gl.define_rotacao (0, 0, 0, 0);
+    gl.desenha_cubo();
 
     glEndList();
     return janela;
