bf923869d427375a3ee5e452c19734b5182b3cc1
geans <geans.santos@gmail.com>
2016-04-06 15:55:44 -0300

    desenha_janela passa a ser método privado para uso no método desenha_predio

--- desenha_janela.cpp
+++ desenha_janela.cpp
@@ -2,14 +2,10 @@
 
 unsigned int Desenho::desenha_janela (float pos_x, float pos_y, float pos_z) {
     float cor_janela[3] = {0x23/256.0, 0x36/256.0, 0x4f/256.0};
-    float janela_largura = 0.05;
+    float janela_largura = 0.15;
     float janela_comprimento = 1.0;
     float janela_altura = 3.0;
     Desenha_gl gl (pos_x, pos_y, pos_z, proporcao);
-
-    /* inicia a composicao da janela */
-    unsigned int janela = glGenLists (1);
-    glNewList (janela, GL_COMPILE);
 
     // janela
     glColor3f ( cor_janela[0], cor_janela[1], cor_janela[2] );
@@ -18,6 +14,5 @@ unsigned int Desenho::desenha_janela (float pos_x, float pos_y, float pos_z) {
     gl.define_rotacao (0, 0, 0, 0);
     gl.desenha_cubo();
 
-    glEndList();
-    return janela;
+    return 0;
 }
